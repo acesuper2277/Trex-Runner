bec75dd8ab7d9c06d8f62d5d56ec33696034534a
acesuper2277 <79986589+acesuper2277@users.noreply.github.com>
2021-03-03 20:28:23 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -78,7 +78,7 @@ function draw() {
     ground.velocityX = -(6 + 3*score/100);
   
     if(keyDown("space") && trex.collide(invisibleGround)) {
-      trex.velocityY = -13;
+      trex.velocityY = -12;
     }
   
     trex.velocityY = trex.velocityY + 0.8
